65f3024aca57700e0939bb9f644eb086142d7ad6
partnersdejoiy-hash <partners.dejoiy@gmail.com>
2026-04-06 01:17:44 +0530

    Update verification.js

--- pages/api/verification.js
+++ pages/api/verification.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.QhGnWW4G_CPx4fP4xPA34LCjCMtrXtFc6);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
 
