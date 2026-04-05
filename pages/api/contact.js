ab209c5655032c73cf0df428ad6d7b29433de3eb
partnersdejoiy-hash <partners.dejoiy@gmail.com>
2026-04-05 23:08:46 +0530

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
     await resend.emails.send({
       from: "DEJOIY <onboarding@resend.dev>",
-      to: ["hello@corp.dejoiy.com"],
+      to: ["partners.dejoiy@gmail.com"],
       subject: "New Contact Form Submission",
       html: `
         <h2>New Lead from DEJOIY Website</h2>
