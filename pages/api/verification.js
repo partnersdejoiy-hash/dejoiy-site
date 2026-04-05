0b010a336a016de84c3712eff6dec3e6b23d78b3
partnersdejoiy-hash <partners.dejoiy@gmail.com>
2026-04-06 01:36:09 +0530

    Update verification.js

--- pages/api/verification.js
+++ pages/api/verification.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
     const response = await resend.emails.send({
       from: `DEJOIY <${process.env.CONTACT_FROM_EMAIL}>`,
-      to: ["employement.verificarion@corp.dejoiy.com"],
+      to: ["employment.verification@corp.dejoiy.com"],
       reply_to: email,
       subject: `New Background Verification Request - ${employeeName}`,
       html: `
